5acc1c4014f908814880fe4e35142b139e95dd4b
provirtys <provirtys@gmail.com>
2021-03-06 11:47:24 +0500

    mk

--- js/script.js
+++ js/script.js
@@ -36,17 +36,55 @@ $(document).ready(function() {
 		}
 		else if($(window).width()>576){
 			$('.news__row-container').removeClass('slider');
-			$('*').removeClass('selected');
+			$('.news *').removeClass('selected');
 			$('.news__arrow').removeClass('disable');
 		}
 		});
 
 	$('.news__arrow').click(function (event) {
-		$('.selected').next().addClass('selected');
-		$('.slider .news__item.selected:first').removeClass('selected');
-		if($('.news__arrow').prev().hasClass('selected')){
-			$(this).addClass('disable');
+		if($('.news__arrow').hasClass('backward')){
+			$('.news__item.selected').prev().addClass('selected');
+			$('.news__item.selected:last').removeClass('selected');
+			if($('.news__item:first').hasClass('selected')){
+				$('.news__arrow').removeClass('backward');
+			}
+		}
+		else{
+			$('.selected').next().addClass('selected');
+			$('.slider .news__item.selected:first').removeClass('selected');
+			if($('.news__arrow').prev().hasClass('selected')){
+				$(this).addClass('backward');
+			}
 		}
 	});
 });
 
+//модальные окна 
+const btns = document.querySelectorAll('.btn');
+const modalOverlay = document.querySelector('.modal-overlay ');
+const modals = document.querySelectorAll('.modal');
+
+btns.forEach((el) => {
+	el.addEventListener('click', (e) => {
+		let path = e.currentTarget.getAttribute('data-path');
+
+		modals.forEach((el) => {
+			el.classList.remove('modal--visible');
+		});
+		document.querySelector('.video').setAttribute('src','https://www.youtube.com/embed/s8J-22E6mpk');
+		document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible');
+		modalOverlay.classList.add('modal-overlay--visible');
+	});
+});
+
+modalOverlay.addEventListener('click', (e) => {
+	console.log(e.target);
+
+	if (e.target == modalOverlay) {
+		modalOverlay.classList.remove('modal-overlay--visible');
+		document.querySelector('.video').setAttribute('src','');
+		modals.forEach((el) => {
+			el.classList.remove('modal--visible');
+		});
+	}
+});
